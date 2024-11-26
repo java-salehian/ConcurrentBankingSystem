896a2e21702ab768d6b74a7810d94a57d26445b1
soha <sha.salehian@gmail.com>
2024-11-26 05:11:16 +0330

    add Transaction Strategy Context

---
+++ src/main/java/azki/vam/banking/service/transaction/TransactionContext.java
@@ -0,0 +1,29 @@
+package azki.vam.banking.service.transaction;
+
+import azki.vam.banking.entity.BankAccount;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+@Component
+public class TransactionContext {
+    private final Map<String, TransactionStrategy> strategyMap;
+    private TransactionStrategy strategy;
+
+    public TransactionContext(Map<String, TransactionStrategy> strategyMap) {
+        this.strategyMap = strategyMap;
+    }
+
+    public void setStrategy(String strategyName) {
+        this.strategy = strategyMap.get(strategyName);
+    }
+
+    public boolean executeStrategy(BankAccount fromAccount, BankAccount toAccount, double amount) {
+        if (strategy != null) {
+            return strategy.execute(fromAccount, toAccount, amount);
+        } else {
+            throw new IllegalArgumentException("Strategy not found");
+        }
+    }
+}
+
